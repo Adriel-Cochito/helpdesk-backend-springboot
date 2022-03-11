671032ba91ed10e4e8d8a71141042031d37de728
Adriel-Cochito <adriel.cochito@al.infnet.edu.br>
2022-03-11 01:00:10 -0300

    Projeto iniciado

---
+++ src/main/java/com/adriel/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.adriel.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
