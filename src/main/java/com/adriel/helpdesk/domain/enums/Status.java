1e9cbd820660e26ce03b1a6a71d6fa9924cd0774
Adriel-Cochito <adriel.cochito@al.infnet.edu.br>
2022-03-12 00:28:38 -0300

    Subindo para o Git Hub

--- src/main/java/com/adriel/helpdesk/domain/enums/Status.java
+++ src/main/java/com/adriel/helpdesk/domain/enums/Status.java
@@ -2,7 +2,8 @@ package com.adriel.helpdesk.domain.enums;
 
 public enum Status {
 	
-	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(2, "ROLE_TECNICO");
+	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+
 	
 	private Integer codigo;
 	private String descricao;
